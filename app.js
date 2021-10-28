731ad320edcfc694c3b322961e8a7810b096b375
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-28 17:54:50 -0300

    changing value price and delete item

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cartValue = document.querySelector('#cart-value');
 const cartModal = document.querySelector('.cart-modal');
 const modalEmpty = document.querySelector('#modal-empty');
 const modalFilled = document.querySelector('#modal-filled');
+const quantity = document.querySelector('#quantity');
+const result = document.querySelector('#result');
 
 function increaseCartNr() {
     ++cartNr.innerText;
@@ -13,10 +15,12 @@ function decreaseCartNr() {
 }
 function addToCart() {
     cartValue.innerText = cartNr.innerText;
+    quantity.innerText = cartNr.innerText;
     cartValue.style.display = 'flex';
     modalEmpty.style.display = 'none';
     modalFilled.style.display = 'none';
     displayCartValue();
+    finalPrice();
 }
 function displayCartValue() {
     if(cartValue.innerText == 0) {
@@ -28,4 +32,13 @@ function displayCartValue() {
 }
 function showCartModal() {
     cartModal.classList.toggle("show-cart");
+}
+function finalPrice() {
+    const calculation = cartNr.innerText * 125;
+    result.innerHTML = `$${calculation}.00`;
+}
+function deleteItem() {
+    cartValue.innerText = 0;
+    modalFilled.style.display = 'none';
+    displayCartValue();
 }
