f856804dd403e9a66f8ca3e0209933e44dbdf608
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-10-28 17:39:09 -0300

    showing and hiding cart content

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const cartNr = document.querySelector('#cart-nr');
 const cartValue = document.querySelector('#cart-value');
+const cartModal = document.querySelector('.cart-modal');
+const modalEmpty = document.querySelector('#modal-empty');
+const modalFilled = document.querySelector('#modal-filled');
+
 function increaseCartNr() {
     ++cartNr.innerText;
 }
@@ -10,11 +14,18 @@ function decreaseCartNr() {
 function addToCart() {
     cartValue.innerText = cartNr.innerText;
     cartValue.style.display = 'flex';
+    modalEmpty.style.display = 'none';
+    modalFilled.style.display = 'none';
     displayCartValue();
 }
 function displayCartValue() {
     if(cartValue.innerText == 0) {
         cartValue.style.display = 'none';
+        modalEmpty.style.display = 'flex';
+    }else{
+        modalFilled.style.display = 'flex';
     }
-    console.log('is working');
+}
+function showCartModal() {
+    cartModal.classList.toggle("show-cart");
 }
